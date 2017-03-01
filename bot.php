dc20cbb7e9e3c25e88acde05ff23d508b13b04ab
ktangudta <ktangudta@gmail.com>
2017-03-01 15:32:59 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'Hey wassup!'
+				'text' => 'Hey wassup!',
     				'type' => 'sticker',
     				'packageId' => '2',
 				'stickerId' => '147'
