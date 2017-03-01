551c0787e9f07c59ea1baf136179431c21f7d11b
ktangudta <ktangudta@gmail.com>
2017-03-01 17:24:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -23,11 +23,11 @@ if (!is_null($events['events'])) {
 				'text' => $text
 			];
 			
-			$message2 = [
-    				'type' => 'sticker',
-    				'packageId' => '2',
-				'stickerId' => '147'
-			];
+			//$message2 = [
+    				//'type' => 'sticker',
+    				//'packageId' => '2',
+				//'stickerId' => '147'
+			//];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
